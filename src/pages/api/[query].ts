422bb8a86ecc5f4d0a16c63b99d7f0a7389ed28f
pugson <hi@wojtek.im>
2023-03-10 04:33:39 +0100

    Initial commit

---
+++ src/pages/api/[query].ts
@@ -0,0 +1,31 @@
+import Cors from "cors";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+// Initializing the cors middleware
+// You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+const cors = Cors({
+  methods: ["POST", "GET", "HEAD"],
+});
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(req: NextApiRequest, res: NextApiResponse, fn: Function) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result: any) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+
+      return resolve(result);
+    });
+  });
+}
+
+export default async function handler(req: any, res: any) {
+  // Run the middleware
+  await runMiddleware(req, res, cors);
+  res.setHeader("Cache-Control", "s-maxage=604800000");
+  const { query } = req.query;
+
+  res.status(200).json({ query });
+}
